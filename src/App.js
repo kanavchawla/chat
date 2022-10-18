e6e42916b7498437d69406a364349aaab50a2818
kanavchawla <kchawla1_be21@thapar.edu>
2022-10-19 01:45:34 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 import React from "react";
-import {usestate} from 'react';
+// import {usestate} from 'react';
+import DirectMessaging from "./dm";
 function App() {
   return (
     <div className="App">
-
+      <DirectMessaging/>
     </div>
   );
 }
-
+// import React from 'react';
+// import { ChatEngine } from 'react-chat-engine';
+//  function App() {
+// 	return (
+// 		<ChatEngine
+// 			height='100vh'
+// 			userName='kanav'
+// 			userSecret='1234'
+// 			projectID='3a6e9754-68ae-424c-917a-61e1c00a8a82'
+// 		/>
+// 	);
+// }
 export default App;
--- src/dm.js
+++ src/dm.js
@@ -1,9 +1,53 @@
 // Project ID: 346eb963-5963-4196-98a5-b2c7b80e7248
-// Private Key: fccab62e-8e66-4755-a791-78ecdf07d37b
-function DM(){
-    return(
-        <>
+// Private Key: 6b9edaba-725c-430c-a003-a91edecd5459
+import React, {useState} from 'react';
+import {ChatEngine, getOrCreateChat} from 'react-chat-engine'
 
-        </>
+const DirectMessaging = () => {
+    // The useState hook initially sets the username to an empty string
+    const[username, setUsername] = useState('')
+    //Custom function that will implement the getOrCreateChat function that to select username to chat with
+    //only when the correct credentials(user  secret, project id, username) are passed will the application be rendered
+    function implementingDirectChat(credentials){
+        getOrCreateChat(
+            credentials,
+            // function will only work if the app is a Direct Messaging one, hence setting 'is_direct_chat' to true and consequentially setting a list of usernames to search from.
+            {is_direct_chat: true, usernames:[username]},
+            () => setUsername('')
+        )
+    }
+
+    const displayChatInterface = (credentials) => {
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder='Find username'
+                    value={username} //prop from the useState hook
+                    // A controlled function that sets the username to what the user types in the input field
+                    onChange = {(e) => setUsername(e.target.value)}
+                />
+
+                {/* clicking button will call the implementingDirectChat function that displays a list of usernames to create or find an existing chat.  */}
+                <button onClick={() => implementingDirectChat(credentials)}>
+                    Create Chat
+                </button>
+
+            </div>
+        )
+    }
+
+    return(
+        <ChatEngine
+            height='100vh'
+            // userName='kanav'
+            // Accessing the stored environment variables in .env file
+            userName='kanav'
+			userSecret='kanav'
+            projectID='3a6e9754-68ae-424c-917a-61e1c00a8a82'
+            displayNewChatInterface={(credentials) => displayChatInterface(credentials)}
+            />
     )
 }
+
+export default DirectMessaging
